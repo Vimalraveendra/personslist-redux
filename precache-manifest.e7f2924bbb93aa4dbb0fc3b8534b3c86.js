fae40ea08265df6befe171c44214119baa7585af
vimal Raveendran <vimalraveendra@gmail.com>
2019-09-06 21:46:39 +0200

    Updates

---
+++ precache-manifest.e7f2924bbb93aa4dbb0fc3b8534b3c86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b108e9bd3f732df545b1c6c768de09",
+    "url": "/personslist-redux/index.html"
+  },
+  {
+    "revision": "3046dc0e4f41d061671e",
+    "url": "/personslist-redux/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "26a17ce7d4dc08205496",
+    "url": "/personslist-redux/static/css/main.f364c752.chunk.css"
+  },
+  {
+    "revision": "3046dc0e4f41d061671e",
+    "url": "/personslist-redux/static/js/2.c67a4d07.chunk.js"
+  },
+  {
+    "revision": "26a17ce7d4dc08205496",
+    "url": "/personslist-redux/static/js/main.32078446.chunk.js"
+  },
+  {
+    "revision": "0b648e470ac992e89ca8",
+    "url": "/personslist-redux/static/js/runtime~main.0ca6c1b8.js"
+  },
+  {
+    "revision": "df868691bca38b437758eab2262299f2",
+    "url": "/personslist-redux/static/media/Lintsec.df868691.woff"
+  }
+]);
