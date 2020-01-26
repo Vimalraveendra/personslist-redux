4d775cf2514a299e5e513496f7a5f70d9b3a161f
vimal Raveendran <vimalraveendra@gmail.com>
2020-01-26 22:15:42 +0100

    updated package dot JSON & also the App component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,40 +1,37 @@
-import React ,{Component}from 'react';
-import PersonsList from '../components/PersonsList';
-import './App.css';
-import {connect} from 'react-redux';
-import { searchPersons} from '../Actions/Actions';
+import React, { Component } from "react";
+import PersonsList from "../components/PersonsList";
+import "./App.css";
+import { connect } from "react-redux";
+import { searchPersons } from "../Actions/Actions";
 
-const mapStateToProps=(state)=>{
-  return{
-    persons:state.persons,
-    isPending:state.isPending,
-    error:state.error
-}
-}
+const mapStateToProps = state => {
+  return {
+    persons: state.persons,
+    isPending: state.isPending,
+    error: state.error
+  };
+};
 
-const mapDispatchToProps= dispatch =>{
-  return{
-    onSearchPersons:()=>dispatch(searchPersons())
-  }
-}
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchPersons: () => dispatch(searchPersons())
+  };
+};
 
 class App extends Component {
-    
-    componentDidMount(){
-     this.props.onSearchPersons()
+  componentDidMount() {
+    this.props.onSearchPersons();
+  }
+  render() {
+    return this.props.isPending ? (
+      <h1>Loading....</h1>
+    ) : (
+      <div>
+        <h1 className="tc">Personal List</h1>
 
-    }
- render(){
-  return (
-    <div >
-      <h1 className='tc'>Personal List</h1>
-
-       <PersonsList  personsArray={this.props.persons}/>
-      
-     
-    </div>
-  );
+        <PersonsList personsArray={this.props.persons} />
+      </div>
+    );
+  }
 }
-}
-export default connect(mapStateToProps,mapDispatchToProps)(App);
-
+export default connect(mapStateToProps, mapDispatchToProps)(App);
