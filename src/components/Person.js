2d8acb7996b2b924f7add6c347f81c35d2798b6a
vimal Raveendran <vimalraveendra@gmail.com>
2020-06-10 21:21:46 +0200

    Updated Person component inline style

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 
-
-const Person = ({firstName,lastName,img,email,city,cell}) =>{
-	return(
-         <div className= 'tc bg-light-green br3 pa3 ma2 grow bw2 shadow-5'>
-              <img className='br3' alt='person'src={img}/>
-           <div>
-            <h2 className="ttu">{firstName} {lastName}</h2>
-            <h3 className="ttc">City:{city}</h3>
-            <p>Email:{email}</p>
-            <p>Phone:{cell}</p>
-            </div>
-          </div>
-
-		)
-}
-
+const Person = ({ firstName, lastName, img, email, city, cell }) => {
+  return (
+    <div className="tc bg-light-green br3 pa3 ma2 grow bw2 shadow-5">
+      <img className="br3" alt="person" src={img} />
+      <div>
+        <h2>
+          {firstName} {lastName}
+        </h2>
+        <h3>City:{city}</h3>
+        <p>Email:{email}</p>
+        <p>Phone:{cell}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Person;
