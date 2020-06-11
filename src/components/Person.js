d06a392dfbb9d590e4b939e7efb7444129b4aabe
vimal Raveendran <vimalraveendra@gmail.com>
2020-06-11 17:08:13 +0200

    Added media queries to images based on the screen size

--- src/components/Person.js
+++ src/components/Person.js
@@ -3,7 +3,12 @@ import React from "react";
 const Person = ({ firstName, lastName, img, email, city, cell }) => {
   return (
     <div className="tc bg-light-green br3 pa3 ma2 grow bw2 shadow-5">
-      <img className="br3" alt="person" src={img} width="200" height="auto" />
+      <img
+        className="br3"
+        alt="person"
+        src={img}
+        sizes="(min-width: 600px) 25vw, (min-width: 500px) 50vw, 100vw"
+      />
       <div>
         <h2>
           {firstName} {lastName}
